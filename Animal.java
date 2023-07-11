1cc6fbac5f7b9379213aa53c1333199b5544c919
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-11 21:53:39 +0500

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,93 @@
+public abstract class Animal {
+
+    private String name, color, kind, type;
+
+    private Double weight, age;
+
+    public Animal(String name, String color, String kind, String type, Double weight, Double age) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Double getAge() {
+        return age;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAge(Double age) {
+        this.age = age;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public void makeSound(String sound) {
+        System.out.println("Sound: " + sound);
+    }
+
+    public void eat(int food) {
+        System.out.println("Have eaten: " + food);
+    }
+
+    public void drink(int water) {
+        System.out.println("Have had water: " + water);
+    }
+
+    public void move(int distance) {
+        System.out.println("Moved to a distance: " + distance);
+    }
+
+    public void sleep(int minutes) {
+        System.out.println("Has slept for " + " minutes");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", kind='" + kind + '\'' +
+                ", type='" + type + '\'' +
+                ", weight=" + weight +
+                ", age=" + age +
+                '}';
+    }
+
+    public abstract void makeSound();
+}
