cc0ac0d115b32b059cad270bf941ec022e00e636
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-11 21:54:20 +0500

    Create Parrot.java

---
+++ Parrot.java
@@ -0,0 +1,22 @@
+public class Parrot extends Animal {
+
+    private String sound;
+
+    public Parrot(String name, String color, String kind, String type, Double weight, Double age) {
+        super(name, color, kind, type, weight, age);
+        this.sound = "Cheeriq cheeriq";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(this.getSound());
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
